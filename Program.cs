67cbb6d27d8cf7f75e2a7375a22603c645743e4f
Johan Fernando Romo Eligio <feryohan343@gmail.com>
2020-09-29 11:21:13 -0500

    primer intento, falta el valor de la fuerza con la distancia obtenida

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeyDeCoulomb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double carga1;
+            double carga2;
+            double radio;
+            double distancia;
+
+            Console.WriteLine("    >>>Programa de Ley de Coulomb<<<     \n\n");
+
+            #region Datos
+            //Se leen los datos del usuario
+            Console.WriteLine("¿\n Cuál es el valor de la carga 1 [microC]? \n");
+            carga1 = byte.Parse(Console.ReadLine());
+
+            Console.WriteLine("¿\n Cuál es el valor de la carga 2? [microC]\n");
+            carga2 = byte.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("¿\n Cuál es la distancia entre la carga 1 y la carga 2? [mm]\n");
+            radio = byte.Parse(Console.ReadLine());
+            #endregion
+
+            distancia = radio/Math.Sqrt(carga1);
+
+        }
+    }
+}
